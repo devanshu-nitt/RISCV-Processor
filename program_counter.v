06360e2e8ef7bc157d43081cf784fbd0fc0366e6
devanshu-nitt <devanshusub2003@gmail.com>
2026-04-16 18:59:00 +0530

    Modify program_counter to include stall input
    Added stall input to control program counter updates.

--- program_counter.v
+++ program_counter.v
@@ -2,10 +2,11 @@ module program_counter #(parameter PC=8)(
 	input wire	clk,
 	input wire	reset,
 	input wire	[PC-1:0]	pc_in,
+	input wire			stall,
 	output reg	[PC-1:0]	pc_out
 );
 	always @(posedge clk) begin	
 		if(reset)	pc_out <= 0;
-		else		pc_out <= pc_in;
+		else if(!stall)	pc_out <= pc_in;
 	end
 endmodule
