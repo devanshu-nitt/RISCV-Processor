457751a8831b8e45904d5b0d923c2be648ab3c5d
devanshu-nitt <devanshusub2003@gmail.com>
2026-04-16 19:07:22 +0530

    Add flush input and modify output assignments in id_stage
    Updated id_stage module to include flush input and modified output assignments accordingly.

--- id_stage.v
+++ id_stage.v
@@ -19,11 +19,15 @@ module id_stage #(	parameter INST = 32,
 	input wire			reg_write_wb,
 	input wire			stall,
 
-	input wire	[WIDTH-1:0]	fwd_ex_result,
-	input wire	[WIDTH-1:0]	fwd_mem_result,
-	input wire	[REG_ADDR-1:0]	rd_ex,
-	input wire			reg_write_ex,
-	input wire			reg_write_mem_fwd,
+	input wire			flush,
+
+	//input wire	[WIDTH-1:0]	fwd_ex_result,
+	//input wire	[WIDTH-1:0]	fwd_mem_result,
+	//input wire	[REG_ADDR-1:0]	rd_ex,
+	//input wire			reg_write_ex,
+	//input wire			reg_write_mem_fwd,
+
+	//input wire	[REG_ADDR-1:0]	rd_wb_pre,
 
 
 	output reg	[WIDTH-1:0]	read_data1_out,
@@ -39,12 +43,14 @@ module id_stage #(	parameter INST = 32,
 	output reg			branch_out,
 	output reg	[REG_ADDR-1:0]	rd_out,
 	output reg	[PC-1:0]	pc_out,
-	output reg	[ADDR-1:0]	rs1_out,
-	output reg	[ADDR-1:0]	rs2_out,
+	output reg	[REG_ADDR-1:0]	rs1_out,
+	output reg	[REG_ADDR-1:0]	rs2_out,
 
 	// Given to IF by ID instead of MEM.
-	output wire			branch_taken,
-	output wire	[PC-1:0]	pc_branch_target
+	output wire			branch_wire,
+	//output wire	[PC-1:0]	pc_branch_target
+	output wire	[REG_ADDR-1:0]	rs1_pre,
+	output wire	[REG_ADDR-1:0]	rs2_pre
 );
 
 	wire	[WIDTH-1:0]	read_data1;
@@ -54,8 +60,8 @@ module id_stage #(	parameter INST = 32,
 	wire			alu_src;
 
 	wire	[OPCODE-1:0]	opcode =instruction[6:0];
-	wire	[ADDR-1:0]	rs1 =instruction[19:15];
-	wire	[ADDR-1:0]	rs2 =instruction[24:20];
+	wire	[REG_ADDR-1:0]	rs1 =instruction[19:15];
+	wire	[REG_ADDR-1:0]	rs2 =instruction[24:20];
 	wire	[REG_ADDR-1:0]	rd_internal =instruction[11:7];
 	wire	[FUNC3-1:0]	func3 =instruction[14:12];
 	wire	[FUNC7-1:0]	func7 =instruction[31:25];
@@ -68,16 +74,21 @@ module id_stage #(	parameter INST = 32,
 	wire			mem_to_reg;
 	wire			branch;
 
-	wire	[WIDTH-1:0]	comp_data1;
-	wire	[WIDTH-1:0]	comp_data2;
+
+	//wire	[WIDTH-1:0]	comp_data1;
+	//wire	[WIDTH-1:0]	comp_data2;
 
 	assign	rd = rd_internal;
 
-	assign	comp_data1 = (reg_write_ex && (rd_ex !=0) && (rd_ex == rs1)) ? fwd_ex_result : (reg_write_mem_fwd && (rd_wb != 0) && (rd_wb == rs1)) ? fwd_mem_result : read_data1;
-	assign	comp_data2 = (reg_write_ex && (rd_ex !=0) && (rd_ex == rs2)) ? fwd_ex_result : (reg_write_mem_fwd && (rd_wb != 0) && (rd_wb == rs2)) ? fwd_mem_result : read_data2;
+	//assign	comp_data1 = (reg_write_ex && (rd_ex !=0) && (rd_ex == rs1)) ? fwd_ex_result : (reg_write_mem_fwd && (rd_wb_pre != 0) && (rd_wb_pre == rs1)) ? fwd_mem_result : read_data1;
+	//assign	comp_data2 = (reg_write_ex && (rd_ex !=0) && (rd_ex == rs2)) ? fwd_ex_result : (reg_write_mem_fwd && (rd_wb_pre != 0) && (rd_wb_pre == rs2)) ? fwd_mem_result : read_data2;
 
-	assign	branch_taken = branch && (comp_data1 == comp_data2);
-	assign	pc_branch_target = pc_in + imm;
+	//assign	branch_taken = branch && (comp_data1 == comp_data2);
+	//assign	pc_branch_target = pc_in + imm;
+
+	assign		branch_wire = branch;
+	assign		rs1_pre = rs1;
+	assign		rs2_pre = rs2;
 
 	control_unit CU(.opcode(opcode), .reg_write(reg_write), .mem_read(mem_read), .mem_write(mem_write), .mem_to_reg(mem_to_reg), .alu_src(alu_src), .branch(branch), .alu_op(alu_op));
 	
@@ -88,7 +99,7 @@ module id_stage #(	parameter INST = 32,
 	alu_control AC(.alu_op(alu_op), .func3(func3), .func7(func7), .alu_ctrl(alu_ctrl));
 
 	always @(posedge clk) begin
-		if(reset || stall) begin
+		if(reset || stall || flush) begin
 			read_data1_out <=0;
 			read_data2_out <=0;
 			imm_out <=0;
@@ -115,7 +126,7 @@ module id_stage #(	parameter INST = 32,
 			mem_read_out <= mem_read;
 			mem_write_out <= mem_write;
 			mem_to_reg_out <= mem_to_reg;
-			branch_out <= branch_taken ? 1'b0 : branch;
+			branch_out <= branch; 
 			rd_out <= rd;
 			rs1_out <= rs1;
 			rs2_out <= rs2;
